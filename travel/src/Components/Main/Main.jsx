f975d00bdcc5b113aeb49a5ddc5303079fa9c9bc
darshi <darshi373@gmail.com>
2023-07-15 15:47:44 +0530

    new

--- travel/src/Components/Main/Main.jsx
+++ travel/src/Components/Main/Main.jsx
@@ -6,6 +6,7 @@ import img3 from './img3.jpg'
 import img4 from './img4.jpg'
 import img5 from './img5.jpg'
 import img6 from './img6.jpg'
+
 import img7 from './img7.jpg'
 import img8 from './img8.jpg'
 import img9 from './img9.jpg'
